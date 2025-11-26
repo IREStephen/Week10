3d80085415e4703311515356b615bc83a2ec6112
StephenDuffy-STUDENT <g00465649@atu.ie>
2025-11-26 17:07:33 +0000

    Added CustomerTest and PreferredCustomer class

---
+++ src/main/java/org/example/CustomerTest.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class CustomerTest {
+    Customer part1Customer = new Customer(
+            "Alice Smith",
+            "1 High Street",
+            "0851234567",
+            1001,
+            true
+    );
+
+}
---
+++ src/main/java/org/example/PreferredCustomer.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class PreferredCustomer extends Customer {
+    private int loyaltyPoints;
+    private double discountLevel; // 0.05 for 5%, etc.
+    // constructors, methods, getters, setters, toString()
+}
